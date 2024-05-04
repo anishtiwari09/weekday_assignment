20d52872170cfa28576bcc5d9d5f4a43f007218e
unknown <aneeshtiwari24@gmail.com>
2024-05-04 17:59:48 +0530

    init

---
+++ src/component/JobSearch.jsx
@@ -0,0 +1,8 @@
+import React, { useRef, useState } from "react";
+const LIMIT = 6;
+export default function JobSearch() {
+  let [lastIndex, setLastIndex] = useState(0);
+  const [allData, setAllData] = useState([]);
+  const fetchAllData = () => {};
+  return <div>Search</div>;
+}
